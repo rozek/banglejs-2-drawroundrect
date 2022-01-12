c31bbbeb40c38d4192c9c2a6f5e1fd0c7f384672
Andreas Rozek <A.Rozek@gmx.de>
2022-01-12 06:33:45 +0100

    Update drawRoundRect.js

--- drawRoundRect.js
+++ drawRoundRect.js
@@ -1,6 +1,6 @@
-(function () {
-  if (g.drawRoundRect == null) {
-    g.drawRoundRect = function drawRoundRect (x1,y1, x2,y2, r) {
+;(function () {
+  if (Graphics.prototype.drawRoundRect == null) {
+    Graphics.prototype.drawRoundRect = function drawRoundRect (x1,y1, x2,y2, r) {
       let x,y;
       if (x1 > x2) { x = x1; x1 = x2; x2 = x; }
       if (y1 > y2) { y = y1; y1 = y2; y2 = y; }
